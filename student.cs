ef399693576fa96bcacfe2835884e3d94b347012
WeiLLLL <128537249+WeiLLLL@users.noreply.github.com>
2023-11-02 22:37:56 -0400

    Create student.cs

---
+++ student.cs
@@ -0,0 +1,20 @@
+namespace SMTIOnlineCourseRegistration.BLL
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public List<Student> GetAllStudents()
+        {
+            return StudentDB.GetAllRecords();
+        }
+
+        public Student SearchStudentById(int userUserId)
+        {
+            return StudentDB.GetStudent(userUserId);
+        }
+    }
+}
